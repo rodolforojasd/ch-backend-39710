05193c9e00b4f792d6424ab74577a169432a27f1
rodolforojasd <rodolfo.rojas.d@gmail.com>
2023-02-17 01:34:02 -0300

    elabore las correciones de los flujos y funciones para que funcionarn los metodos

--- Desafios/desafio-01/script.js
+++ Desafios/desafio-01/script.js
@@ -3,22 +3,24 @@ import { v4 as uuidv4 } from 'uuid'
 
 class ProductManager {
     constructor(){
-        this.products = [];
+        this.products = []
     }
 
     getProducts(){
-        return this.products;
+        console.log(this.products)
+        return this.products
     }
 
     addProduct(title,description,price,thumbnail,stock){
         const product = new Product (0,title,description,price,thumbnail,0,stock)
         
         const productByCode = this.products.find((el) => el.code === product.code)
+        const productByTitle = this.products.find((el) => el.title === product.title)
 
         if (this.products.length === 0 && isNaN(product.price) === false && isNaN(product.stock ) === false){
             product.id = 1
             this.products.push(product)
-        }else if  (this.products.length > 0 && productByCode ==='undefined' && isNaN(product.price) === false && isNaN(product.stock ) === false){
+        }else if  (this.products.length > 0 && productByCode === undefined && productByTitle === undefined && isNaN(product.price) === false && isNaN(product.stock ) === false){
             product.id = this.products[this.products.length -1].id + 1
             this.products.push(product)
         }else {
@@ -28,7 +30,7 @@ class ProductManager {
 
     getProductById(id){
         const productById = this.products.find((product) => product.id === id)
-        if (productById !== 'undefined'){
+        if (productById !== undefined){
             console.log(productById)
         }else {
             console.log('Not found.')
@@ -60,7 +62,6 @@ productManager.addProduct('producto de prueba','Este es un producto de prueba',2
 productManager.getProducts()
 
 productManager.addProduct('producto de prueba','Este es un producto de prueba',200,'Sin imagen',25)
-
 productManager.getProductById(1)
 productManager.getProductById(2)
 productManager.getProducts()
